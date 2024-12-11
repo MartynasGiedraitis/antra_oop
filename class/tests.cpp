588cdb4c22c193d86f2a12357c372689ca13bb57
MartynasGiedraitis <martynui07@gmail.com>
2024-12-11 20:37:53 +0200

    minor updates

--- class/tests.cpp
+++ class/tests.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include "studentas.h"
 #include "mylib.h"
+#include <sstream>
 
 TEST(StudentTest, CalculateAverage){
     std::vector<int> pazymiai = {8,9,7};
@@ -13,12 +14,19 @@ TEST(StudentTest, CalculateAverage){
     EXPECT_DOUBLE_EQ(stud.getVid(), expected_vid);
 
 }
+TEST(StudentTest, PrintInfo) {
+    
+    Student stud("Jonas", "Jonaitis", 8, {7, 9, 10});
+    stud.skaiciuotiVidurki();
+    std::ostringstream oss;
+    
+    stud.printInfo(oss);
+ 
+    std::string expected_output = "Jonas           Jonaitis        8.27      ";  // Pakeiskite pagal tikrąją reikšmę
+    EXPECT_EQ(oss.str(), expected_output);
+}
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);  // Inicijuoja Google Test
     return RUN_ALL_TESTS();  // Paleidžia visus testus
 }
-// TEST_CASE("Testuojame Student klasės egzamino nustatymą"){
-//     Student stud;
-//     stud.setEgz(10);
-//     REQUIRE(stud.getEgz() == 10);
-// }
